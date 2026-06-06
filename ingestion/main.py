e375aba0c2dd4b4dc156ff04bb99e6199a07d312
Mayara Melo <124809927+maysmelo@users.noreply.github.com>
2026-06-06 18:03:48 +0200

    Final adjustments to the code

--- ingestion/main.py
+++ ingestion/main.py
@@ -1,13 +1,13 @@
 #raw data ingestion of Data API Breed's into GCP and Bigquery
+import io
 import json
 import requests
-import pandas as pd
 from datetime import datetime, timezone
-from google.cloud import storage, biquery
+from google.cloud import storage, bigquery, secretmanager
 
 #IDS
 PROJECT_ID = "cogent-treat-409814" #project name at gcp
-BUCKET_NAME = "heyra-dog-raw" #bucket of gcp
+BUCKET_NAME = "heyra_dog_raw" #bucket of gcp
 DATASET_ID = "bronze" #bigquery dataset
 TABLE_ID = "dog_api_raw" #bigquery table name
 API_URL = "https://api.thedogapi.com/v1/breeds" #API URL
@@ -18,8 +18,8 @@ def get_secret(secret_id):
     response = client.access_secret_version(request={"name": name})
     return response.payload.data.decode("UTF-8")
 
-#defining the api function
-def ingesting_api(request):
+#defining the api function - important for gcloud!
+def ingest_dog_breeds(request):
     #defining the calling date
     run_date = datetime.now(timezone.utc).strftime("%Y-%m-%d")
     #api key 
