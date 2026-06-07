c59bb21c4ec3c9f2dd111088c22a81095d3bf0fc
Mayara Stelmaschuk Melo <maystelmaschuk@gmail.com>
2026-06-07 18:19:17 +0000

    Adding all columns to gold

--- models/marts/dim_breed.sql
+++ models/marts/dim_breed.sql
@@ -4,23 +4,7 @@ with source as (
 ),
 final as (
     select
-    breed_id,
-    breed_name,
-    breed_group,
-    origin,
-    country_code,
-    temperament,
-    description,
-    history,
-    height_male_cm,
-    height_female_cm,
-    height_male_in,
-    height_female_in,
-    weight_male_kg,
-    weight_female_kg,
-    weight_male_lbs,
-    weight_female_lbs
+    *
 from source
 )
-
 select * from final
