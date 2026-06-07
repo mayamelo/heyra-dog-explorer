1e9b78101ab50d9b627b3c5ea9ae0bf8c1a2d60d
Mayara Stelmaschuk Melo <maystelmaschuk@gmail.com>
2026-06-07 15:12:10 +0000

    Modified the macro to properly flag the production schemas

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,9 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
-    {%- elif target.name == 'prod' -%}
-        {{ custom_schema_name }}
-    {%- else -%}
+    {%- elif target.schema in ('silver_dev', 'gold_dev') -%}
         {{ custom_schema_name }}_dev
+    {%- else -%}
+        {{ custom_schema_name }}
     {%- endif -%}
 {%- endmacro %}
